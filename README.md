d61860c7796e69a2bfc4684080990916a7ab5ce7
Chris McGowan <chris@anthum.com>
2018-03-20 09:23:30 -0400

    read buffer now used for decoding

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-This repo is **incomplete** and will provide examples for streaming audio with the new Fetch &amp; Streams APIs.
+This repo is **incomplete/in-progress** and will provide examples for streaming audio with the new Fetch &amp; Streams APIs.
 
 Development will occur in the following phases:
 
 1. **WAV Decoding - Proof of Concept** &nbsp; ✅ *complete*<br>Streaming local WAV files works as intended.  Remote files don't stream proplerly due to the chunks received that contain odd (not even) number of total bytes.
-1. **WAV Decoding - Buffered Read** &nbsp; 😶 *incomplete*<br>Bytes read will be scheduled into a read buffer to prevent failed chunk decoding resulting from incomplete chunks
+1. **WAV Decoding - Buffered Read** &nbsp; ✅ *complete*<br>Bytes read are scheduled into a read buffer to prevent failed chunk decoding resulting from incomplete chunks.
+1. **WAV Decoding - Web Worker** &nbsp; 😶 *incomplete*<br>Decoding will be moved to a Web Worker
 1. **Opus Decoding** &nbsp; 😶 *incomplete*<br>WebAssembly will be used to decode [Opus](http://opus-codec.org/) files.  (MP3 is outdated).  This would simulate a real-world use case of streaming compressed audio over the web.
 
 # Acknowledgements
--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
 'use strict'
 
-// Remote files won't decode properly until enqueue() implements controlled read buffer scheduling
-fetch('https://fetch-stream-audio.anthum.com/300kbps/rock-48000hz-trim.wav')
+// fetch('https://fetch-stream-audio.anthum.com/nolimit/rock-48000hz-trim.wav')
+// fetch('https://fetch-stream-audio.anthum.com/300kbps/rock-48000hz-trim.wav')
+fetch('https://fetch-stream-audio.anthum.com/200kbps/rock-48000hz-trim.wav')
+// fetch('https://fetch-stream-audio.anthum.com/192kbps/rock-48000hz-trim.wav')
+// fetch('https://fetch-stream-audio.anthum.com/180kbps/rock-48000hz-trim.wav')
 // fetch('https://fetch-stream-audio.anthum.com/300kbps/rock-48000hz-trim-mono.wav')
 // fetch('https://fetch-stream-audio.anthum.com/300kbps/house-41000hz-trim.wav')
 // fetch('https://fetch-stream-audio.anthum.com/300kbps/house-41000hz-trim-mono.wav')
 
-/* localhost files should decode without issue */
-// fetch('https://fetch-stream-audio.local.com/audio/rock-48000hz-trim.wav')
-// fetch('https://fetch-stream-audio.local.com/audio/house-41000hz-trim.wav')
+/* localhost */
+// fetch('audio/rock-48000hz-trim.wav')
+// fetch('audio/house-41000hz-trim.wav')
 .then(response => {
   if (!response.ok) throw Error(response.status+' '+response.statusText)
   if (!response.body) throw Error('ReadableStream not yet supported in this browser - <a href="https://developer.mozilla.org/en-US/docs/Web/API/Body/body#Browser_Compatibility">browser compatibility</a>')
@@ -17,14 +20,15 @@ fetch('https://fetch-stream-audio.anthum.com/300kbps/rock-48000hz-trim.wav')
         contentLength = response.headers.get('content-length'), // requires CORS access-control-expose-headers: content-length
         bytesTotal = contentLength? parseInt(contentLength, 10) : 0
 
-  // debug iterations
-  let reads = 0, bytesRead = 0;
+  let bytesRead = 0;
 
   read()
   function read() {
     reader.read().then(({value, done}) => {
-      if (!done) {
-        reads++, bytesRead+= value.byteLength;
+      if (done) {
+        AudioPlayer.flushBuffer();
+      } else {
+        bytesRead+= value.byteLength;
         UI.downloadProgress({bytesRead, bytesTotal})
         AudioPlayer.enqueue(value);
         read();
@@ -39,80 +43,119 @@ const AudioPlayer = (function() {
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)(),
         decoder = new MohayonaoWavDecoder();
 
-  let delayNextStart = .3, // timing of next audio buffer.  initialize with intentional delay
-      playStarted,          // did playback start
-      abCreated = 0;        // AudioBuffers created
+  /* bytes received must be buffered to ensure decoder receives complete chunks.
+   * Otherwise, decoder returns white noise (typically for odd (not even) chunk size).
+   * Skipping occurs if too small or if network isn't fast enough.
+   * Users must wait too long to hear audio if too large.
+   */
+  const abSize = 16*1024,
+        readBuffer = new Uint8Array(abSize);
 
-  // TODO - this function must schedule a read buffer be a certain size before flushing to decoder. Otherwise, bad decoding occurs resulting in white noise / hissing for those decoded chuncks
-  function enqueue(typedArray) {
-    // white noise will be decoded odd numbers of bytes
-    if (typedArray.byteLength % 2) {
-      console.log(abCreated+1, 'AudioBuffer contains odd bytes', typedArray.byteLength % 2, typedArray.byteLength);
+  let totalTimeScheduled = 0,   // time scheduled of all AudioBuffers
+      playStartedAt = 0,        // audioContext.currentTime of first scheduled play buffer
+      abCreated = 0,            // AudioBuffers created
+      abEnded = 0,              // AudioBuffers played/ended
+      bufferPos = 0;            // readBuffer position (to insert)
+
+  function flushBuffer() {
+    const chunk = readBuffer.slice(0,bufferPos),
+          decodeStart = performance.now();
+
+    decoder.decodeChunk(chunk)
+    .then(data => {
+      console.debug('decoded',chunk.byteLength,'bytes in', (performance.now()-decodeStart).toFixed(2)+'ms');
+      schedulePlayback(data)
+    });
+
+    bufferPos = 0;
+  }
+
+  function enqueue(streamChunk) {
+    updateUI()
+
+    for (let i=0; i<streamChunk.byteLength; i++) {
+      readBuffer[bufferPos++] = streamChunk[i];
+
+      // flush readBuffer to decoder if full
+      if (bufferPos === readBuffer.byteLength) {
+        flushBuffer();
+      }
     }
+  }
 
-    decoder.decodeChunk(typedArray)
-    .then(data => playDecodedData(data));
+  function updateUI() {
+    UI.bufferUpdate({abSize, abCreated, abEnded});
   }
 
   function togglePause() {
     if(audioCtx.state === 'running') {
-      audioCtx.suspend()
-      return true;
+      return audioCtx.suspend().then(_ => true)
     } else if(audioCtx.state === 'suspended') {
-      audioCtx.resume()
-      return false;
+      return audioCtx.resume().then(_ => false)
     }
   }
 
-  function playDecodedData({channelData, length, numberOfChannels, sampleRate}) {
+  function schedulePlayback({channelData, length, numberOfChannels, sampleRate}) {
+    if (!playStartedAt) {
+      playStartedAt = audioCtx.currentTime + (audioCtx.baseLatency || 0.1);
+      UI.playing();
+    }
+
     const audioSrc = audioCtx.createBufferSource();
+
+    audioSrc.addEventListener('ended', function() {
+      abEnded++;
+      updateUI();
+    })
+
     const audioBuffer = new AudioBuffer({
       length,
       numberOfChannels,
       sampleRate
     });
     abCreated++;
-    UI.bufferUpdate({abCreated});
+    updateUI();
 
     for (let i=0; i<numberOfChannels; i++) {
       audioBuffer.copyToChannel(channelData[i], i)
     }
-    // console.log('audioBuffer', audioBuffer)
-
     audioSrc.buffer = audioBuffer
     audioSrc.connect(audioCtx.destination);
-    audioSrc.start(delayNextStart);
-    delayNextStart+= audioBuffer.duration;
-    if (!playStarted) {
-      playStarted = true;
-      UI.playing()
-    }
+
+    audioSrc.start(playStartedAt+totalTimeScheduled);
+    totalTimeScheduled+= audioBuffer.duration;
   }
 
   return {
     enqueue,
+    flushBuffer,
     togglePause
   }
 })()
 
 
+
 const UI = (function() {
-  let elStatus, elProgress, elAbCreated
+  let elStatus, elProgress, elAbSize, elAbCreated, elAbEnded, elAbRemaining;
 
   // Pause/Resume with space bar
   document.onkeydown = event => {
     if (event.code === 'Space') {
-      if (AudioPlayer.togglePause())
-        paused()
-      else
-        playing()
+      AudioPlayer.togglePause()
+      .then(isPaused => {
+        if (isPaused) paused()
+        else playing()
+      })
     }
   }
 
-  document.addEventListener('DOMContentLoaded', evt => {
+  document.addEventListener('DOMContentLoaded', _ => {
     elStatus = document.getElementById('status');
     elProgress = document.getElementById('progress');
+    elAbSize = document.getElementById('abSize');
     elAbCreated = document.getElementById('abCreated');
+    elAbEnded = document.getElementById('abEnded');
+    elAbRemaining = document.getElementById('abRemaining');
   })
 
 
@@ -124,8 +167,11 @@ const UI = (function() {
     }
   }
 
-  function bufferUpdate({abCreated}) {
+  function bufferUpdate({abSize, abCreated, abEnded}) {
+    elAbSize.innerHTML = abSize.toLocaleString()+ ' ('+abSize/1024+'K)';
     elAbCreated.innerHTML = abCreated;
+    elAbEnded.innerHTML = abEnded;
+    elAbRemaining.innerHTML = abCreated-abEnded;
   }
 
   function error(content) {
